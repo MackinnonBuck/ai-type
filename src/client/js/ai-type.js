1c8ab26e9173c6bcfbba5e7299399d46300a8bd5
Mackinnon Buck <mackinnon.buck@gmail.com>
2024-06-29 10:41:49 -0700

    Basic server

--- src/client/js/ai-type.js
+++ src/client/js/ai-type.js
@@ -1,1 +1,45 @@
-console.log('Hello, world!');
+const aiTypeTargetId = 'ai-type-target';
+const origin = document.location.origin;
+
+function activate() {
+    const element = document.getElementById(aiTypeTargetId);
+    if (!element) {
+        console.error(`Could not find the element with ID ${aiTypeTargetId}`);
+    }
+
+    element.addEventListener('keydown', (ev) => {
+        if (ev.ctrlKey && ev.key === ' ') {
+            ev.preventDefault();
+            attachSuggestionOverlay(element);
+        }
+    });
+}
+
+/**
+ * 
+ * @param {HTMLTextAreaElement} textAreaElement 
+ */
+async function attachSuggestionOverlay(textAreaElement) {
+    // const overlay = document.createElement('div');
+    // textAreaElement.addEventListener('')
+    // const obj = {
+    //     name: 'bob',
+    //     age: 54,
+    // };
+
+    // JSON.stringify(obj);
+    const response = await fetch(`${origin}/api/autocomplete`, {
+        body: JSON.stringify({
+            text: textAreaElement.value,
+        }),
+        headers: {
+            ['Content-Type']: 'application/json',
+        },
+        method: 'POST',
+    });
+
+    const responseText = await response.text();
+    console.log(responseText);
+}
+
+activate();
--- src/server/main.py
+++ src/server/main.py
@@ -1,1 +1,31 @@
-print('Hello, world!')
+from flask import Flask, redirect, request, url_for, send_from_directory
+
+app = Flask(__name__)
+
+# Test endpoint
+@app.route('/helloworld')
+def hello_world():
+    return 'Hello, world!'
+
+# Playground endpoints
+@app.route('/')
+def root():
+    return redirect(url_for('playground_home'))
+
+@app.route('/playground')
+def playground_home():
+    return send_from_directory('../client', 'index.html')
+
+@app.route('/playground/<path:path>')
+def playground_file(path):
+    return send_from_directory('../client', path)
+
+# API endpoints
+@app.route('/api/autocomplete', methods=['POST'])
+def autocomplete():
+    content = request.json
+    text = content['text']
+    return f'The value you supplied is {text}'
+
+if __name__ == '__main__':
+    app.run()
